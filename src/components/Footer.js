9d25c41c0a3b0cef66465228ce55500655bd2509
ch1kulya <164895536+ch1kulya@users.noreply.github.com>
2025-08-11 11:56:33 +0300

    minor update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ const Footer = {
     view: () =>
         m('footer', [
             m('span', 'Powered by ', m('a', { href: 'https://shikimori.one', target: '_blank' }, 'Shikimori')),
-            m('span.footer-separator', '//'),
+            m('span.footer-separator', ''),
             m('span', '© 2025 ch1ka'),
-            m('span.footer-separator', '//'),
+            m('span.footer-separator', ''),
             m('span', 'Source code on ', m('a', { href: 'https://github.com/ch1kulya/saku', target: '_blank' }, 'GitHub')),
         ]),
 };
